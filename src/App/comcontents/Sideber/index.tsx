43e582c5520cfa08fbd43c32e7980f9109ca7087
ryota-murakami <dojce1048@gmail.com>
2022-07-18 12:38:14 +0900

    link mock

--- src/App/comcontents/Sideber/index.tsx
+++ src/App/comcontents/Sideber/index.tsx
@@ -1,7 +1,14 @@
 import React, { memo } from 'react'
+import { Link } from 'react-router-dom'
 
 const Sidebar: React.FC = () => {
-  return <section>Sidebar</section>
+  return (
+    <section className="w-16 p-4">
+      <Link to="/d1">D1</Link>
+      <Link to="/d2">D2</Link>
+      <Link to="/d2">D3</Link>
+    </section>
+  )
 }
 
 export default memo(Sidebar)
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,11 +4,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Index from './pages/Index'
 
+//@TODO
+const Dummy1 = () => <li>Dummy1</li>
+const Dummy2 = () => <li>Dummy2</li>
+const Dummy3 = () => <li>Dummy3</li>
+
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />}></Route>
+        <Route path="/d1" element={<Dummy1 />} />
+        <Route path="/d2" element={<Dummy2 />} />
+        <Route path="/d3" element={<Dummy3 />} />
+        <Route path="/" element={<Index />} />
       </Routes>
     </BrowserRouter>
   )
