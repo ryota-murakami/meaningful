a6460ac5318a97d7b67a9d760aa66aff98238703
ryota-murakami <dojce1048@gmail.com>
2023-07-06 14:58:55 +0900

    up

--- src/App/pages/Index/index.tsx
+++ src/App/pages/Index/index.tsx
@@ -1,9 +1,19 @@
 import React, { memo } from 'react'
 
 import logo from '../../../logo.svg'
+import { enqueSnackbar } from '../../../Profiler/redux/snackbarSlice'
+import { dispatch } from '../../../Profiler/redux/store'
 import Layout from '../../comcontents/Layout/index'
 
 const Index: React.FC = memo(() => {
+  dispatch(
+    enqueSnackbar({
+      componentName: '<Index />',
+      message: 'render <Index />',
+      variant: 'primary',
+    })
+  )
+
   return (
     <section>
       <img src={logo} className="App-logo" alt="logo" />
